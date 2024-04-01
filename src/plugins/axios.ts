806e8e4b56e90dd39505af7ce472979f6597a75b
tangguhriyadi <mtangguh97@gmail.com>
2024-04-01 20:20:43 +0700

    change BASE_URL

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -1,6 +1,6 @@
 import Axios from "axios";
 
-const BASE_URL = "http://localhost:4000/api/v1";
+const BASE_URL = "https://bookstore-be-omega.vercel.app/api/v1";
 
 const axios = Axios.create({
     baseURL: BASE_URL,
